bfff50a7c21a198cbe6be2ba0cf705178210f9e9
Mahmoud <mahmoudnasser1561@gmail.com>
2026-01-02 22:43:24 +0200

    variables

--- variables.tf
+++ variables.tf
@@ -15,3 +15,34 @@ variable "env" {
   type        = string
   default     = "dev"
 }
+
+variable "my_ip" {
+  description = "IP for bastion SSH"
+  type        = string
+  default     = "156.199.83.23/32"
+}
+
+variable "frontend_port" {
+  type    = number
+  default = 80
+}
+
+variable "backend_port" {
+  type    = number
+  default = 8080
+}
+
+variable "db_port" {
+  type    = number
+  default = 3306
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  type        = string
+  default     = "" 
+}
