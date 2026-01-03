7960a38ccdc02cc02518f63c26c191b6c0014f28
Mahmoud <mahmoudnasser1561@gmail.com>
2026-01-03 21:26:01 +0200

    removed frontend metric to monitoing module

--- modules/frontend/main.tf
+++ modules/frontend/main.tf
@@ -61,18 +61,3 @@ resource "aws_autoscaling_policy" "scale_out" {
   cooldown               = 300
   autoscaling_group_name = aws_autoscaling_group.frontend.name
 }
-
-resource "aws_cloudwatch_metric_alarm" "high_cpu" {
-  alarm_name          = "frontend-high-cpu"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 2
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = 120
-  statistic           = "Average"
-  threshold           = 70
-  alarm_actions       = [aws_autoscaling_policy.scale_out.arn]
-  dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.frontend.name
-  }
-}
